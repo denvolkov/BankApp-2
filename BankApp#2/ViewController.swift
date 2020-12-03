2debd105a278aac9e32137f8c9df3c6d95a70d13
denvolkov <denvolkov@inbox.ru>
2020-12-03 16:17:59 +0300

    Initial Commit

---
+++ BankApp#2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankApp#2
+//
+//  Created by test on 03.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
